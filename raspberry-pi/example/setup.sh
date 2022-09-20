044176eb0a8d1d677b6484a31a16281910e81a4c
Shirley Leung <shirley@Shirleys-MacBook-Pro.local>
2022-09-19 21:55:01 -0400

    fixed typo in shell

--- raspberry-pi/example/setup.sh
+++ raspberry-pi/example/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pip3 install -r requirements.txt 
-sudo apt-get -Y install libportaudio0 
-sudo apt-get -Y install libportaudio2 
-sudo apt-get -Y install libportaudiocpp0 
-sudo apt-get -Y install portaudio19-dev
-sudo apt-get -Y install mpg123
+sudo apt-get -y install libportaudio0 
+sudo apt-get -y install libportaudio2 
+sudo apt-get -y install libportaudiocpp0 
+sudo apt-get -y install portaudio19-dev
+sudo apt-get -y install mpg123
